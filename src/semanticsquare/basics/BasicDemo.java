38b110dd2db6a7aeb86adc6fd6aa74fe99072a40
rirvin <rirvin@apexclearing.com>
2020-06-25 16:52:55 -0700

    completed through first assignment

---
+++ src/semanticsquare/basics/BasicDemo.java
@@ -0,0 +1,10 @@
+// Java classes, methods, and variables must start with letter, underscore $. 
+
+
+class BasicsDemo {
+	int id = 0;
+	
+	public static void main(String[] args) {
+		System.out.println(1 +2);
+	}
+}
